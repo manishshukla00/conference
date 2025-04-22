4663e8ec2493a206ba92ffc5bb329840dbb799a2
sanjeevalg <skgcp86@gmail.com>
2025-04-22 09:01:07 +0530

    f43

--- src/pages/AuthorGuidlines.jsx
+++ src/pages/AuthorGuidlines.jsx
@@ -8,7 +8,7 @@ const AuthorGuidlines = () => {
         <div className="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400 p-1 rounded-lg shadow-lg">
           <div className="bg-white rounded-lg p-6">
             <h2 className="text-3xl font-extrabold text-purple-800 mb-6 text-center">
-              Author Guidelines
+              Submission
             </h2>
             <p className="text-lg text-gray-800 mb-4">
               We are excited to welcome authors to submit their technical papers
@@ -80,6 +80,13 @@ const AuthorGuidlines = () => {
                 <span className="text-gray-500">Link coming soon...</span>
               </li>
             </ul>
+            <p>
+              Regular Papers: between 4 to 5 pages maximum (2 additional pages
+              allowed but at an extra charges). Regular papers should present
+              novel perspectives within the general scope of the conference. A
+              very limited number of short papers (between 2 to 3 pages) can be
+              accepted based on the review and conference chair’s decision
+            </p>
 
             <h3 className="text-2xl font-bold text-purple-700 mt-6 mb-4">
               Author's Guidelines:
